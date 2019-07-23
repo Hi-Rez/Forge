601a662195882f790334d64fa0fee4ab56a415c4
rezaali <syed.reza.ali@gmail.com>
2019-07-23 16:59:22 -0700

    Initial Commit

---
+++ Forge/Forge.h
@@ -0,0 +1,19 @@
+//
+//  Forge.h
+//  Forge
+//
+//  Created by Reza Ali on 7/23/19.
+//  Copyright © 2019 Reza Ali. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Forge.
+FOUNDATION_EXPORT double ForgeVersionNumber;
+
+//! Project version string for Forge.
+FOUNDATION_EXPORT const unsigned char ForgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Forge/PublicHeader.h>
+
+
