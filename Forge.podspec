4d88b173484237e50ffbce46400afd8e6e7e6903
rezaali <syed.reza.ali@gmail.com>
2019-08-22 12:30:08 -0700

    updated Renderer

---
+++ Forge.podspec
@@ -0,0 +1,26 @@
+Pod::Spec.new do |spec|
+  spec.name                   = "Forge"
+  spec.version                = "0.0.2"
+  spec.summary                = "Get up and rendering with Metal via Metalkit without worrying about triple buffering / semaphores"
+  spec.description            = <<-DESC
+  Forge's Renderer class sets up triple buffering rendering so you don't have to. You also get nice Cinder / Processing / Openframeworks functions you can hook into and do stuff.
+                   DESC
+  spec.homepage               = "https://github.com/Hi-Rez/Forge"
+  spec.license                = { :type => "MIT", :file => "LICENSE" }
+  spec.author                 = { "Reza Ali" => "reza@hi-rez.io" }
+  spec.social_media_url       = "https://twitter.com/rezaali"
+  spec.source                 = { :git => "https://github.com/Hi-Rez/Forge.git", :tag => spec.version.to_s }
+  spec.osx.deployment_target  = "10.14"
+  spec.ios.deployment_target  = "12.4"
+  spec.tvos.deployment_target = "12.4"
+
+  spec.osx.source_files       = "Forge/*.h", "Forge/Shared/**/*.{h,m,swift}", "Forge/macOS/**/*.{h,m,swift}"
+  spec.ios.source_files       = "Forge/*.h", "Forge/Shared/**/*.{h,m,swift}", "Forge/iOS/**/*.{h,m,swift}"
+  spec.tvos.source_files      = "Forge/*.h", "Forge/Shared/**/*.{h,m,swift}", "Forge/tvOS/**/*.{h,m,swift}"
+
+  spec.osx.resources          = "Forge/macOS/*.xib"
+
+  spec.frameworks             = "Metal", "MetalKit"
+  spec.module_name            = "Forge"
+  spec.swift_version          = "5.1"
+end
--- Forge/Shared/Renderer.swift
+++ Forge/Shared/Renderer.swift
@@ -16,9 +16,17 @@ open class Renderer: NSObject, MTKViewDelegate {
     public weak var mtkView: MTKView!
     public let device: MTLDevice
     public let commandQueue: MTLCommandQueue
-    public var sampleCount: Int = 1
-    public var colorPixelFormat: MTLPixelFormat = .bgra8Unorm
-    public var depthStencilPixelFormat: MTLPixelFormat = .invalid
+    public var sampleCount: Int {
+        return self.mtkView.sampleCount
+    }
+    
+    public var colorPixelFormat: MTLPixelFormat {
+        return self.mtkView.colorPixelFormat
+    }
+    
+    public var depthStencilPixelFormat: MTLPixelFormat {
+        return self.mtkView.depthStencilPixelFormat
+    }
     
     let inFlightSemaphore = DispatchSemaphore(value: maxBuffersInFlight)
     
@@ -29,14 +37,18 @@ open class Renderer: NSObject, MTKViewDelegate {
         guard let queue = self.device.makeCommandQueue() else { return nil }
         self.commandQueue = queue
         
-        metalKitView.depthStencilPixelFormat = self.depthStencilPixelFormat
-        metalKitView.colorPixelFormat = self.colorPixelFormat
-        metalKitView.sampleCount = self.sampleCount
-        
         super.init()
+        
+        metalKitView.depthStencilPixelFormat = .depth32Float_stencil8
+        metalKitView.colorPixelFormat = .bgra8Unorm_srgb
+        metalKitView.sampleCount = 1
+        
+        self.setupMtkView(self.mtkView)
         
         self.setup()
     }
+    
+    open func setupMtkView(_ metalKitView: MTKView) {}
     
     open func draw(in view: MTKView) {
         guard let commandBuffer = self.preDraw() else { return }
@@ -49,7 +61,7 @@ open class Renderer: NSObject, MTKViewDelegate {
     }
     
     deinit { self.cleanup() }
-        
+    
     open func preDraw() -> MTLCommandBuffer? {
         self.update()
         
@@ -81,35 +93,35 @@ open class Renderer: NSObject, MTKViewDelegate {
     open func resize(_ size: (width: Float, height: Float)) {}
     
     open func cleanup() {}
-#if os(macOS)
-    open func touchesBegan(with event: NSEvent) {}
-    
-    open func touchesEnded(with event: NSEvent) {}
-    
-    open func touchesMoved(with event: NSEvent) {}
-    
-    open func touchesCancelled(with event: NSEvent) {}
-    
-    open func scrollWheel(with event: NSEvent) {}
-    
-    open func mouseMoved(with event: NSEvent) {}
-    
-    open func mouseDown(with event: NSEvent) {}
-    
-    open func mouseDragged(with event: NSEvent) {}
-    
-    open func mouseUp(with event: NSEvent) {}
-    
-    open func mouseEntered(with event: NSEvent) {}
-    
-    open func mouseExited(with event: NSEvent) {}
-    
-    open func keyDown(with event: NSEvent) {}
-    
-    open func keyUp(with event: NSEvent) {}
+    #if os(macOS)
+        open func touchesBegan(with event: NSEvent) {}
         
-    open func magnify(with event: NSEvent) {}
-    
-    open func rotate(with event: NSEvent) {}
-#endif
+        open func touchesEnded(with event: NSEvent) {}
+        
+        open func touchesMoved(with event: NSEvent) {}
+        
+        open func touchesCancelled(with event: NSEvent) {}
+        
+        open func scrollWheel(with event: NSEvent) {}
+        
+        open func mouseMoved(with event: NSEvent) {}
+        
+        open func mouseDown(with event: NSEvent) {}
+        
+        open func mouseDragged(with event: NSEvent) {}
+        
+        open func mouseUp(with event: NSEvent) {}
+        
+        open func mouseEntered(with event: NSEvent) {}
+        
+        open func mouseExited(with event: NSEvent) {}
+        
+        open func keyDown(with event: NSEvent) {}
+        
+        open func keyUp(with event: NSEvent) {}
+        
+        open func magnify(with event: NSEvent) {}
+        
+        open func rotate(with event: NSEvent) {}
+    #endif
 }
