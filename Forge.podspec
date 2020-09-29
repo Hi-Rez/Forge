b22b574547f7cedc6d98075c5292d647be2f5ed8
rezaali <syed.reza.ali@gmail.com>
2020-09-29 15:23:43 -0700

    added ForgeView to help with SwiftUI support, bumped up Forge min version for iOS/tvOS to 13 & macOS to 10.15

--- Forge.podspec
+++ Forge.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name                   = "Forge"
-  spec.version                = "1.0.3"
+  spec.version                = "1.0.4"
   spec.summary                = "Get up and rendering with Metal via Metalkit without worrying about triple buffering / semaphores"
   spec.description            = <<-DESC
   Forge's Renderer class sets up triple buffering rendering so you don't have to. You also get nice Cinder / Processing / Openframeworks functions you can hook into and do stuff.
@@ -10,9 +10,9 @@ Pod::Spec.new do |spec|
   spec.author                 = { "Reza Ali" => "reza@hi-rez.io" }
   spec.social_media_url       = "https://twitter.com/rezaali"
   spec.source                 = { :git => "https://github.com/Hi-Rez/Forge.git", :tag => spec.version.to_s }
-  spec.osx.deployment_target  = "10.14"
-  spec.ios.deployment_target  = "12.4"
-  spec.tvos.deployment_target = "12.4"
+  spec.osx.deployment_target  = "10.15"
+  spec.ios.deployment_target  = "13.0"
+  spec.tvos.deployment_target = "13.0"
 
   spec.osx.source_files       = "Forge/*.h", "Forge/Shared/**/*.{h,m,swift}", "Forge/macOS/**/*.{h,m,swift}"
   spec.ios.source_files       = "Forge/*.h", "Forge/Shared/**/*.{h,m,swift}", "Forge/iOS/**/*.{h,m,swift}"
@@ -22,7 +22,7 @@ Pod::Spec.new do |spec|
   spec.ios.resources          = "Forge/iOS/*.xib"
   spec.tvos.resources         = "Forge/tvOS/*.xib"
 
-  spec.frameworks             = "Metal", "MetalKit"
+  spec.frameworks             = "Metal", "MetalKit", "SwiftUI"
   spec.module_name            = "Forge"
   spec.swift_version          = "5.1"
 end
---
+++ Forge/Shared/ForgeView.swift
@@ -0,0 +1,71 @@
+//
+//  ForgeView.swift
+//  Forge
+//
+//  Created by Reza Ali on 9/29/20.
+//
+
+import SwiftUI
+
+#if os(macOS)
+public struct ForgeView: NSViewControllerRepresentable {
+    public var renderer: Forge.Renderer
+    public typealias NSViewControllerType = Forge.ViewControllerpublic
+    
+    public init(renderer: Forge.Renderer) {
+        self.renderer = renderer
+    }
+    
+    public func makeNSViewController(context: Self.Context) -> Self.NSViewControllerType {
+        let vc = Forge.ViewController(nibName: .init("ViewController"), bundle: Bundle(for: Forge.ViewController.self))
+        vc.renderer = renderer
+        return vc
+    }
+    
+    public func updateNSViewController(_ nsViewController: Self.NSViewControllerType, context: Self.Context) {
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    public class Coordinator: NSObject {
+        var parent: ForgeView
+        init(_ parent: ForgeView) {
+            self.parent = parent
+        }
+    }
+}
+
+#else
+
+public struct ForgeView: UIViewControllerRepresentable {
+    public var renderer: Forge.Renderer
+    public typealias UIViewControllerType = Forge.ViewController
+        
+    public init(renderer: Forge.Renderer) {
+        self.renderer = renderer
+    }
+    
+    public func makeUIViewController(context: Self.Context) -> Self.UIViewControllerType {
+        let vc = Forge.ViewController(nibName: .init("ViewController"), bundle: Bundle(for: Forge.ViewController.self))
+        vc.renderer = renderer
+        return vc
+    }
+    
+    public func updateUIViewController(_ uiViewController: Self.UIViewControllerType, context: Self.Context) {
+    }
+
+    public func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    public class Coordinator: NSObject {
+        var parent: ForgeView
+        init(_ parent: ForgeView) {
+            self.parent = parent
+        }
+    }
+}
+
+#endif
